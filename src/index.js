a952389037fc9e0043a4d90363959d39e00f232e
Andrew Bresee <andrew.bresee@smartthings.com>
2018-02-07 08:11:13 -0800

    Implment class-based components and start using Youtube API
    This patch reimplements components as classes instead of functions.
    It also starts using the YouTube search library YTSearch

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 // Create a new component
 import React from 'react'
 import ReactDom from 'react-dom'
+import YTSearch from 'youtube-api-search'
 
 import config from '../config'
+import SearchBar from './components/search_bar'
 const API_KEY = config.youtubeAPI
 
+YTSearch({key: API_KEY, term: 'surfboards'}, (data) => console.log(data))
+
 const App = () => {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+);
 }
 
 // Take the generate HTML and add it to the DOM
